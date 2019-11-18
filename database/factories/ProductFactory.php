02c070b140f5295029a1f202c21a07945e588f47
Christian Camilo Camargo Vega <ccamargov20@gmail.com>
2019-11-17 20:08:44 -0500

    [ADD] Se agrega API RestFull para consultar los productos y su respectiva cantidad disponibles.

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -5,6 +5,8 @@ use Faker\Generator as Faker;
 
 $factory->define(Product::class, function (Faker $faker) {
   return [
-    'name' => $faker->text($maxNbChars = 40)  
+    'name' => $faker->text($maxNbChars = 40),
+    'quantity' => $faker->randomDigit,
+    'availableDate' => $faker->date($format = 'Y-m-d', $max = 'now')
   ];
 });
